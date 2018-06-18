36f9e72fee9def44a97d56a0c7bb4153832b25ff
AwayQuEM <awayquym@gmail.com>
2018-06-18 23:15:03 +0800

    initialize workspace

---
+++ b/xdispatchDemo/xdispatchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xdispatchDemo
+//
+//  Created by Away on 2018/6/18.
+//  Copyright © 2018 Away. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ b/xdispatchDemo/xdispatchDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  xdispatchDemo
+//
+//  Created by Away on 2018/6/18.
+//  Copyright © 2018 Away. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
